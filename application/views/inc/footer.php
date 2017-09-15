7544e4ef7fb9339af687f495c81e2d2257a77316
Maco Cortes <maco.techdepot@gmail.com>
2017-09-15 18:13:12 +0800

    updated footer

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -1,5 +1,5 @@
 <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
-    </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    <strong>&copy; 2017 <a href="#">St. Vincent College - Dipolog</a></strong>. All rights
     reserved.
+</div>     
+    Developed by <strong>Group 1</strong>
