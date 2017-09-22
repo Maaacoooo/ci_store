2b8e341ab95ad16e8af8fd9df2509dcbe4f345b5
Maco Cortes <maco.techdepot@gmail.com>
2017-09-23 02:01:22 +0800

    updated breadcrumbs

--- application/views/dashboard/dashboard_user.php
+++ application/views/dashboard/dashboard_user.php
@@ -36,8 +36,6 @@
         <?=$title?>        
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Examples</a></li>
         <li class="active"><?=$title?></li>
       </ol>
     </section>
