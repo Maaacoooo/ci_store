a0498c3922c4bb4ce3bb752fe6f7794ebb1614dc
Maco Cortes <maco.techdepot@gmail.com>
2017-09-26 20:33:15 +0800

    Added Page Render data

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -1,5 +1,6 @@
 <div class="pull-right hidden-xs">
     <strong>&copy; 2017 <a href="#">St. Vincent College - Dipolog</a></strong>. All rights
     reserved.
-</div>     
-    Developed by <strong>Group 1</strong>
+</div>     	
+    <span class="label bg-blue">{elapsed_time}s</span>
+	<span class="label bg-green">{memory_usage}</span>
