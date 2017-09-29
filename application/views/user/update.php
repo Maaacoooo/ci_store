559c83019c5549f7c82db29c3fc696cf688e3e7b
Maco Cortes <maco.techdepot@gmail.com>
2017-09-30 06:47:14 +0800

    Updated Label

--- application/views/user/update.php
+++ application/views/user/update.php
@@ -280,7 +280,7 @@
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">×</span></button>
-                <h4 class="modal-title">Delete <?=$info['name']?></h4>
+                <h4 class="modal-title">Reset Password: <?=$info['name']?></h4>
               </div>
               <div class="modal-body">
                 <p>Are you sure to reset to DEFAULT PASSWORD?</p>
